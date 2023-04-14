f46b9c787ae08b60234de892dba9ef79f555b707
Mousasow1907 <130715265+Mousasow1907@users.noreply.github.com>
2023-04-14 15:36:19 -0500

    Update insfollow.sh

--- insfollow.sh
+++ insfollow.sh
@@ -894,7 +894,7 @@ sleep 3
 
 done
 printf " \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 60 secs...\e[0m\n"
-sleep 600
+sleep 300
 #unfollow
 for celeb in $(cat celeb_id); do
 data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
